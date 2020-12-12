95f70229f19e5d36480d1ac3cf57e865e8558cd1
Jesper Uddström <jesper@uddstrom.se>
2020-12-12 10:17:09 +0100

    Day 6

---
+++ 2020/Day06/main.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+const getUnionCount = (group) => {
+    return [...new Set(group.flat())].length;
+};
+
+const getIntersectCount = (group) => {
+    const intersect = (a, b) => a.filter((el) => b.includes(el));
+    return group.reduce((acc, curr) => intersect(acc, curr)).length;
+};
+
+const getGroups = (input) => {
+    const groups = [];
+    let group = [];
+    input.forEach((line) => {
+        if (line === '') {
+            groups.push(group);
+            group = [];
+        } else {
+            group.push(line.split(''));
+        }
+    });
+    return groups;
+};
+
+fs.readFile('Day06/puzzle_input', 'utf8', function (err, contents) {
+    const puzzle_input = [...contents.split('\r\n'), ''];
+
+    const unionSum = getGroups(puzzle_input)
+        .map(getUnionCount)
+        .reduce((acc, curr) => acc + curr, 0);
+
+    const intersectSum = getGroups(puzzle_input)
+        .map(getIntersectCount)
+        .reduce((acc, curr) => acc + curr, 0);
+
+    console.log('Part 1:', unionSum);
+    console.log('Part 2:', intersectSum);
+});

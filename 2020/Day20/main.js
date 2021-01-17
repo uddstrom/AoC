eab040b8c77ed62b80d769455dd3abb77b70a05e
Jesper Uddström <jesper@uddstrom.se>
2021-01-17 16:32:11 +0100

    Day 20 part 2 WIP

--- 2020/Day20/main.js
+++ 2020/Day20/main.js
@@ -5,7 +5,7 @@ const Tile = (id) => ({
     data: [],
 });
 
-const setEdgeValues = (tile) => {
+const setBoarderValues = (tile) => {
     const toDecimals = (binaryString) => [
         parseInt(binaryString, 2),
         parseInt(binaryString.split('').reverse().join(''), 2),
@@ -15,8 +15,8 @@ const setEdgeValues = (tile) => {
     const [bottomFlipped, bottom] = toDecimals(tile.data[9]);
     const [leftFlipped, left] = toDecimals(tile.data.map(d => d.charAt(0)).join(''));
     const [right, rightFlipped] = toDecimals(tile.data.map(d => d.charAt(9)).join(''));
-    tile.edges = [top, bottom, left, right];
-    tile.edgesFlipped = [topFlipped, bottomFlipped, leftFlipped, rightFlipped];
+    tile.borders = [top, bottom, left, right];
+    tile.bordersFlipped = [topFlipped, bottomFlipped, leftFlipped, rightFlipped];
 };
 
 const getTiles = (data) => {
@@ -33,24 +33,52 @@ const getTiles = (data) => {
             tiles.push(tile);
         }
     });
-    tiles.forEach(tile => setEdgeValues(tile));
+    tiles.forEach(tile => setBoarderValues(tile));
     return tiles;
 };
 
 const findCorners = (tiles) => {
     return tiles.filter(tile => {
-        const findMatchingTile = (edge) => {
+        const findMatchingTile = (boarder) => {
             return tiles
                 .filter(t => t.id !== tile.id)
-                .find(t => t.edges.includes(edge) || t.edgesFlipped.includes(edge));
+                .find(t => t.borders.includes(boarder) || t.bordersFlipped.includes(boarder));
         };
-        const matches = tile.edges.map((edge) => findMatchingTile(edge)).filter(m => m !== undefined);
-        const matchesFlipped = tile.edgesFlipped.map((edge) => findMatchingTile(edge)).filter(m => m !== undefined);
+        const matches = tile.borders.map((border) => findMatchingTile(border)).filter(m => m !== undefined);
+        const matchesFlipped = tile.bordersFlipped.map((boarder) => findMatchingTile(boarder)).filter(m => m !== undefined);
         return matches.length < 3 && matchesFlipped.length < 3;
     }).map(tile => tile.id);
 };
 
-fs.readFile('Day20/puzzle_input', 'utf8', function (err, contents) {
+const matchTiles = (tiles) => {
+    tiles.forEach(tile => {
+        console.log(tile);
+        const findMatchingTile = (boarder) => {
+            return tiles
+                .filter(t => t.id !== tile.id)
+                .find(t => t.borders.includes(boarder) || t.bordersFlipped.includes(boarder));
+        };
+        tile.top = findMatchingTile(tile.borders[0]) || findMatchingTile(tile.bordersFlipped[0]);
+        tile.bottom = findMatchingTile(tile.borders[1]) || findMatchingTile(tile.bordersFlipped[1]);
+        tile.left = findMatchingTile(tile.borders[2]) || findMatchingTile(tile.bordersFlipped[2]);
+        tile.right = findMatchingTile(tile.borders[3]) || findMatchingTile(tile.bordersFlipped[3]);
+    });
+};
+
+fs.readFile('Day20/input', 'utf8', function (err, contents) {
     const tiles = getTiles(contents);
     console.log('Part 1:', findCorners(tiles).reduce((acc, id) => acc * id));
+
+    /*
+    Find the tile order
+    Remove the borders
+    Search for sea monsters
+    Count # not part of the sea monster
+    */
+
+    // Find the tile order
+    matchTiles(tiles);
+
+
+    console.log(tiles);
 });

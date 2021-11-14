b45a062f44e35a22e7b589d580e50d9d99689652
Jesper Uddström <jesper@uddstrom.se>
2021-11-14 20:12:57 +0100

    Day 4

--- 2018/lib/utils.js
+++ 2018/lib/utils.js
@@ -5,11 +5,32 @@ function getData(path) {
     const contents = fs.readFileSync(path, 'utf8');
     return function parseContent(parser) {
         return parser(contents);
-    }
+    };
+}
+
+function getMostFrequentElement(array) {
+    return array
+        .slice(0)
+        .sort(
+            (a, b) =>
+                array.filter((val) => val === a).length -
+                array.filter((val) => val === b).length
+        )
+        .pop();
 }
 
 function range(start, end) {
-    return Array(end - start + 1).fill().map((_, idx) => start + idx)
+    return Array(end - start + 1)
+        .fill()
+        .map((_, idx) => start + idx);
 }
 
-module.exports = { getData, range }
+function toDate(str) {
+    // 1518-07-07 00:50
+    var [dateStr, timeStr] = str.split(' ');
+    var [YYYY, MM, dd] = dateStr.split('-').map(Number);
+    var [hh, mm] = timeStr.split(':').map(Number);
+    return new Date(Date.UTC(YYYY, MM - 1, dd, hh, mm));
+}
+
+module.exports = { getData, getMostFrequentElement, range, toDate };

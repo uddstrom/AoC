c8f84bb5042a20c0b8a9f151224a12d68a94e461
Jesper Uddström <jesper@uddstrom.se>
2021-11-23 20:58:13 +0100

    use puzzle inpit as grid

--- 2019/Day18/main.js
+++ 2019/Day18/main.js
@@ -1,9 +1,7 @@
 import { getData, getPath } from '../lib/utils.js';
 import aStar from '../lib/aStar.js';
 
-const PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/puzzle_input_test`;
-const ROWS = 15;
-const COLS = 30;
+const PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/puzzle_input`;
 
 var parser = (input) => {
     return input.split('\n').map((row) => row.split(''));
@@ -16,9 +14,9 @@ function print(maze) {
 function printGrid(grid, path) {
     var maze = grid.map((row, r) =>
         row.map((col, c) => {
-            if (col.wall) return '#';
-            else if (inPath(r, c, path)) return '*';
-            else return ' ';
+            if (inPath(r, c, path)) return '*';
+            if (col.tile === '.') return ' ';
+            return col.tile;
         })
     );
     print(maze);
@@ -37,8 +35,10 @@ function makeGrid(rows, cols) {
     return g;
 }
 
-const node = (row, col) => {
-    var wall = Math.random() < 0.2;
+function node(row, col, tile) {
+    var wall = '#'.includes(tile);
+    var door = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.includes(tile);
+    var key = 'abcdefghijklmnopqrstuvwxyz'.includes(tile);
     return {
         row,
         col,
@@ -47,10 +47,13 @@ const node = (row, col) => {
         neighbors: [],
         previous: undefined,
         wall,
+        door,
+        key,
+        tile,
     };
-};
+}
 
-const addNeighbors = (row, col, grid) => {
+function addNeighbors(row, col, grid) {
     var neighbors = [];
     if (row > 0 && !grid[row - 1][col].wall) neighbors.push(grid[row - 1][col]);
     if (row < grid.length - 1 && !grid[row + 1][col].wall)
@@ -59,10 +62,20 @@ const addNeighbors = (row, col, grid) => {
     if (col < grid[row].length - 1 && !grid[row][col + 1].wall)
         neighbors.push(grid[row][col + 1]);
     return neighbors;
-};
+}
+
+function find(tile, grid) {
+    for (var row of grid) {
+        for (var col of row) {
+            if (col.tile === tile) return col;
+        }
+    }
+}
 
 const main = async () => {
     var maze = getData(PUZZLE_INPUT_PATH)(parser);
+    const ROWS = maze.length;
+    const COLS = maze[0].length;
     // print(maze);
     // console.log('Part 1:');
     // console.log('Part 2:');
@@ -72,24 +85,18 @@ const main = async () => {
     };
 
     var grid = makeGrid(ROWS, COLS).map((row, r) =>
-        row.map((col, c) => node(r, c))
+        row.map((_, c) => node(r, c, maze[r][c]))
     );
-
-    var start = grid[0][0];
-    var goal = grid[ROWS - 1][COLS - 1];
-    start.wall = false;
-    goal.wall = false;
-
     grid.forEach((row, r) =>
-        row.forEach(
-            (col, c) => (grid[r][c].neighbors = addNeighbors(r, c, grid))
-        )
+        row.forEach((_, c) => (grid[r][c].neighbors = addNeighbors(r, c, grid)))
     );
+
+    var start = find('@', grid);
+    var goal = find('z', grid);
 
     var path = aStar(start, goal, h);
 
     printGrid(grid, path);
-    // console.log(path);
 };
 
 main();

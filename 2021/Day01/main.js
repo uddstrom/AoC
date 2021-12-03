e53ab572d9a3e6b0f548b6fe6d88cd2e0396a7be
Jesper Uddström <jesper@uddstrom.se>
2021-12-03 14:59:17 +0100

    cleanup

--- 2021/Day01/main.js
+++ 2021/Day01/main.js
@@ -1,25 +1,26 @@
 import { getData, getPath } from '../lib/utils.js';
 
-const PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/puzzle_input`;
+var PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/puzzle_input`;
 
 var parser = (input) => {
     return input.split('\n').map(Number);
 };
 
-
 function countIncreases(data) {
-    return data.reduce((acc, curr, idx, arr) => curr > arr[idx-1] ? acc + 1 : acc, 0);
+    return data.reduce(
+        (acc, curr, idx, arr) => (curr > arr[idx - 1] ? acc + 1 : acc),
+        0
+    );
 }
 
 function sumOfThree(data) {
-    return data.map((el ,idx, arr) => (el + arr[idx - 1] + arr[idx - 2]));
+    return data.map((el, idx, arr) => el + arr[idx - 1] + arr[idx - 2]);
 }
 
-const main = async () => {
+function main() {
     var puzzle = getData(PUZZLE_INPUT_PATH)(parser);
-
     console.log('Part 1:', countIncreases(puzzle));
     console.log('Part 2:', countIncreases(sumOfThree(puzzle)));
-};
+}
 
 main();
--- 2021/Day02/main.js
+++ 2021/Day02/main.js
@@ -1,6 +1,6 @@
 import { getData, getPath } from '../lib/utils.js';
 
-const PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/puzzle_input`;
+var PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/puzzle_input`;
 
 var parser = (input) => {
     return input
@@ -10,27 +10,33 @@ var parser = (input) => {
 };
 
 function part1(commands) {
-    var endPos = commands.reduce(({ h, v }, [dir, val]) => {
-        if (dir === 'forward') return { h: h + val, v };
-        if (dir === 'up') return { h, v: v - val };
-        if (dir === 'down') return { h, v: v + val };
-    }, { h:0, v:0 });
+    var endPos = commands.reduce(
+        ({ h, v }, [dir, val]) => {
+            if (dir === 'forward') return { h: h + val, v };
+            if (dir === 'up') return { h, v: v - val };
+            if (dir === 'down') return { h, v: v + val };
+        },
+        { h: 0, v: 0 }
+    );
     return endPos.h * endPos.v;
 }
 
 function part2(commands) {
-    var endPos = commands.reduce(({ h, v, aim }, [dir, val]) => {
-        if (dir === 'forward') return { h: h + val, v: v + val * aim, aim };
-        if (dir === 'up') return { h, v, aim: aim - val };
-        if (dir === 'down') return { h, v, aim: aim + val };
-    }, { h:0, v:0, aim: 0 });
+    var endPos = commands.reduce(
+        ({ h, v, aim }, [dir, val]) => {
+            if (dir === 'forward') return { h: h + val, v: v + val * aim, aim };
+            if (dir === 'up') return { h, v, aim: aim - val };
+            if (dir === 'down') return { h, v, aim: aim + val };
+        },
+        { h: 0, v: 0, aim: 0 }
+    );
     return endPos.h * endPos.v;
 }
 
-const main = async () => {
+function main() {
     var commands = getData(PUZZLE_INPUT_PATH)(parser);
     console.log('Part 1:', part1(commands));
     console.log('Part 2:', part2(commands));
-};
+}
 
 main();

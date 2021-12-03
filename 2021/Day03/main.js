991e78792a978e9a91edd17e6ee55baa5000da96
Jesper Uddström <jesper@uddstrom.se>
2021-12-03 14:52:57 +0100

    refactor to remove code duplications

--- 2021/Day03/main.js
+++ 2021/Day03/main.js
@@ -1,6 +1,6 @@
 import { getData, getPath } from '../lib/utils.js';
 
-const PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/puzzle_input`;
+var PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/puzzle_input`;
 
 var parser = (input) => {
     return input
@@ -9,60 +9,51 @@ var parser = (input) => {
         .map((arr) => arr.map(Number));
 };
 
-function mostCommon(data, bit) {
-    var sum = data.reduce((acc, curr) => acc + curr[bit], 0);
-    return sum >= data.length / 2 ? 1 : 0;
-}
-
-function leastCommon(data, bit) {
-    var sum = data.reduce((acc, curr) => acc + curr[bit], 0);
-    return sum < Math.ceil(data.length / 2) ? 1 : 0;
-}
-
-function gamma(data) {
-    var strGamma = '';
-    for (var i = 0; i < data[0].length; i++) {
-        strGamma += mostCommon(data, i);
-    }
-    return parseInt(strGamma, 2);
-}
-
-function epsilon(data) {
-    var strGamma = '';
-    for (var i = 0; i < data[0].length; i++) {
-        strGamma += leastCommon(data, i);
-    }
-    return parseInt(strGamma, 2);
-}
-
-function oxygen(data, bit = 0) {
-    if (data.length === 1) return parseInt(data[0].join(''), 2);
-    var most = mostCommon(data, bit);
-    return oxygen(
-        data.filter((val) => val[bit] === most),
-        bit + 1
-    );
-}
-
-function co2(data, bit = 0) {
-    if (data.length === 1) return parseInt(data[0].join(''), 2);
-    var most = leastCommon(data, bit);
-    return co2(
-        data.filter((val) => val[bit] === most),
-        bit + 1
-    );
-}
-
-const main = async () => {
-    var data = getData(PUZZLE_INPUT_PATH)(parser);
-
-    var gammaRate = gamma(data);
-    var epsilonRate = epsilon(data);
-    var oxygenRate = oxygen(data);
-    var co2Rate = co2(data);
-
-    console.log('Part 1:', gammaRate * epsilonRate);
-    console.log('Part 2:', oxygenRate * co2Rate);
+var getBitBy = (predicate) => (bytes, pos) => {
+    var frequency = bytes.reduce((acc, curr) => acc + curr[pos], 0);
+    return predicate(frequency) ? 1 : 0;
 };
+
+var mostCommon = (bytes) => {
+    var mostCommonPredicate = (frequency) => frequency >= bytes.length / 2;
+    return (pos) => getBitBy(mostCommonPredicate)(bytes, pos);
+};
+
+var leastCommon = (bytes) => {
+    var leastCommonPredicate = (frequency) => frequency < bytes.length / 2;
+    return (pos) => getBitBy(leastCommonPredicate)(bytes, pos);
+};
+
+function rateBy(predicate) {
+    return function rate(bytes, bStr = '', pos = 0) {
+        var byteSize = bytes[0].length;
+        return pos > byteSize - 1
+            ? parseInt(bStr, 2)
+            : rate(bytes, `${bStr}${predicate(bytes)(pos)}`, pos + 1);
+    };
+}
+
+function filterBy(predicate) {
+    return function filter(bytes, pos = 0) {
+        return bytes.length === 1
+            ? parseInt(bytes[0].join(''), 2)
+            : filter(
+                  bytes.filter((byte) => byte[pos] === predicate(bytes)(pos)),
+                  pos + 1
+              );
+    };
+}
+
+function main() {
+    var bytes = getData(PUZZLE_INPUT_PATH)(parser);
+
+    var gamma = rateBy(mostCommon);
+    var epsilon = rateBy(leastCommon);
+    var oxygen = filterBy(mostCommon);
+    var co2 = filterBy(leastCommon);
+
+    console.log('Part 1:', gamma(bytes) * epsilon(bytes));
+    console.log('Part 2:', oxygen(bytes) * co2(bytes));
+}
 
 main();

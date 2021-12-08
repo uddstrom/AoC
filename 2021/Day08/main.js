5807272706d44ef97d077a7039c28a446f5ef412
Jesper Uddström <jesper@uddstrom.se>
2021-12-08 23:06:57 +0100

    Day 8 (#1)
    * wip
    
    * day 8 done

---
+++ 2021/Day08/main.js
@@ -0,0 +1,65 @@
+import { generatePermutations, getData, getPath, sum } from '../lib/utils.js';
+
+var PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/puzzle_input`;
+
+function parser(input) {
+    return input
+        .split('\n')
+        .map((line) => line.split(' | '))
+        .map((line) => ({
+            signals: line[0].split(' '),
+            output: line[1].split(' '),
+        }));
+}
+
+let _wireConfigurations = generatePermutations(['a', 'b', 'c', 'd', 'e', 'f', 'g']);
+let _segmentsMap = new Map([
+    ['012456', 0],
+    ['25', 1], //       0000
+    ['02346', 2], //   1    2
+    ['02356', 3], //   1    2
+    ['1235', 4], //     3333
+    ['01356', 5], //   4    5
+    ['013456', 6], //  4    5
+    ['025', 7], //      6666
+    ['0123456', 8],
+    ['012356', 9],
+]);
+
+function numberConverter(config) {
+    // the config param represents the signal wires to segments configuration
+    // config: eg 'deafgbc'
+    return function (signal) {
+        // signal: eg 'cdfbe'
+        // cdfbe = [60351] => sort => [01356] = 5
+        let segmentCombination = signal
+            .split('')
+            .map((chr) => config.indexOf(chr))
+            .sort()
+            .join('');
+        return _segmentsMap.get(segmentCombination);
+    };
+}
+
+function decode({ signals, output }) {
+    let isValid = (n) => n !== undefined;
+    for (let config of _wireConfigurations) {
+        let toNumber = numberConverter(config);
+        let signalValues = signals.map(toNumber);
+        if (signalValues.every(isValid)) {
+            return Number(output.map(toNumber).join(''));
+        }
+    }
+}
+
+function main() {
+    let entries = getData(PUZZLE_INPUT_PATH)(parser);
+
+    let numberToCount = (str) => [2, 3, 4, 7].includes(str.length);
+    let countNumbers = ({ output }) => output.filter(numberToCount).length;
+
+    console.log('Part 1:', sum(entries.map(countNumbers)));
+    console.log('Part 2:', sum(entries.map(decode)));
+}
+
+main();
--- 2021/lib/utils.js
+++ 2021/lib/utils.js
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { not } from './fn.js';
 
 export function getData(path) {
     var contents = fs.readFileSync(path, 'utf8');
@@ -40,4 +41,36 @@ export function max(arr) {
 
 export function sum(arr) {
     return arr.reduce((acc, curr) => acc + curr, 0);
+}
+
+export function isOdd(n) {
+    return n % 2 === 1;
+}
+
+export var isEven = not(isOdd);
+
+// Generate all permutations of an array A,
+// stores them in the set S and returns S.
+export function generatePermutations(array) {
+    function swap(arr, i, j) {
+        var b = arr[j];
+        arr[j] = arr[i];
+        arr[i] = b;
+    }
+
+    function recursiveHeap(k, A, S) {
+        if (k === 1) {
+            S.add(A.join(''));
+        } else {
+            recursiveHeap(k - 1, A, S);
+            for (var i = 0; i < k - 1; i++) {
+                if (isEven(k)) swap(A, i, k - 1);
+                else swap(A, 0, k - 1);
+                recursiveHeap(k - 1, A, S);
+            }
+        }
+    }
+    let S = new Set();
+    recursiveHeap(array.length, array.slice(), S);
+    return S;
 }

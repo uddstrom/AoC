370908cc1471e9fc09e8305638f5ef25dd30a773
Jesper Uddström <jesper@uddstrom.se>
2021-12-17 20:59:53 +0100

    Day 16

--- 2021/Day16/main.js
+++ 2021/Day16/main.js
@@ -6,6 +6,7 @@ function parser(input) {
 }
 
 let hexToBin = (hex) => parseInt(hex, 16).toString(2).padStart(4, '0');
+let b2i = (bStr) => parseInt(bStr, 2);
 
 function* streamReader(stream) {
     let bitsToRead = 0;
@@ -19,11 +20,9 @@ function* streamReader(stream) {
 }
 
 let versions = [];
-let b2i = (bStr) => parseInt(bStr, 2);
 
 function readPacket(reader, readXtra = true) {
-    let version = reader.next(3).value;
-    versions.push(version);
+    versions.push(reader.next(3).value);
     let type = b2i(reader.next(3).value);
     let bitCnt = 6;
     if (type === 4) {
@@ -36,34 +35,26 @@ function readPacket(reader, readXtra = true) {
             bitCnt += 5;
         }
         let xtraBits = readXtra ? 4 - (bitCnt % 4) : 0;
-        let { value, done } = reader.next(xtraBits);
+        let { done } = reader.next(xtraBits);
         return [b2i(values.join('')), done];
     } else {
         // operator packet,
         let [subPacks, done] = getSubPacks(reader, bitCnt, readXtra);
         let [s1, s2] = subPacks;
-        switch (type) {
-            case 0: // sum
-                return [sum(subPacks), done];
-            case 1: // product
-                return [product(subPacks), done];
-            case 2: // min
-                return [Math.min(...subPacks), done];
-            case 3: // max
-                return [Math.max(...subPacks), done];
-            case 5: // gtn
-                return s1 > s2 ? [1, done] : [0, done];
-            case 6: // ltn
-                return s1 < s2 ? [1, done] : [0, done];
-            case 7: // eq
-                return s1 === s2 ? [1] : [0];
-        }
+        if (type === 0) return [sum(subPacks), done];
+        if (type === 1) return [product(subPacks), done];
+        if (type === 2) return [Math.min(...subPacks), done];
+        if (type === 3) return [Math.max(...subPacks), done];
+        if (type === 5) return s1 > s2 ? [1, done] : [0, done];
+        if (type === 6) return s1 < s2 ? [1, done] : [0, done];
+        if (type === 7) return s1 === s2 ? [1, done] : [0, done];
     }
 }
 
 function getSubPacks(reader, bitCnt, readXtra) {
     let lengthTypeId = reader.next(1).value;
     bitCnt++;
+    let subPacks = [];
     if (lengthTypeId === '0') {
         // the next 15 bits are a number that represents the total length in bits of the sub-packets contained by this packet
         let subPacketsTotalLength = b2i(reader.next(15).value);
@@ -74,31 +65,25 @@ function getSubPacks(reader, bitCnt, readXtra) {
 
         let subReader = streamReader(subPacketsBits);
         subReader.next();
-        let subPacks = [];
         let more = true;
         while (more) {
             let [subPack, done] = readPacket(subReader, false);
             subPacks.push(subPack);
             more = !done;
         }
-
-        let xtraBits = readXtra ? 4 - (bitCnt % 4) : 0;
-        let { value, done } = reader.next(xtraBits);
-        return [subPacks, done];
     }
     if (lengthTypeId === '1') {
         // the next 11 bits are a number that represents the number of sub-packets immediately contained by this packet
         let numberOfSubPackets = b2i(reader.next(11).value);
         bitCnt += 11;
 
-        let subPacks = range(0, numberOfSubPackets - 1)
+        subPacks = range(0, numberOfSubPackets - 1)
             .map((_) => readPacket(reader, false))
             .map(([subPack, done]) => subPack);
-
-        let xtraBits = readXtra ? 4 - (bitCnt % 4) : 0;
-        let { value, done } = reader.next(xtraBits);
-        return [subPacks, done];
     }
+    let xtraBits = readXtra ? 4 - (bitCnt % 4) : 0;
+    let { done } = reader.next(xtraBits);
+    return [subPacks, done];
 }
 
 function main() {

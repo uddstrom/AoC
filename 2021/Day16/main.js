b8390943099e363faa5c45f41ea6b98e02cb6aa9
Jesper Uddström <jesper@uddstrom.se>
2021-12-17 21:35:30 +0100

    refactor

--- 2021/Day16/main.js
+++ 2021/Day16/main.js
@@ -21,34 +21,18 @@ function* streamReader(stream) {
 
 let versions = [];
 
-function readPacket(reader, readXtra = true) {
-    versions.push(reader.next(3).value);
-    let type = b2i(reader.next(3).value);
-    let bitCnt = 6;
-    if (type === 4) {
-        // literal packet, read values
-        let values = [];
-        let last = false;
-        while (!last) {
-            last = reader.next(1).value === '0';
-            values.push(reader.next(4).value);
-            bitCnt += 5;
-        }
-        let xtraBits = readXtra ? 4 - (bitCnt % 4) : 0;
-        let { done } = reader.next(xtraBits);
-        return [b2i(values.join('')), done];
-    } else {
-        // operator packet,
-        let [subPacks, done] = getSubPacks(reader, bitCnt, readXtra);
-        let [s1, s2] = subPacks;
-        if (type === 0) return [sum(subPacks), done];
-        if (type === 1) return [product(subPacks), done];
-        if (type === 2) return [Math.min(...subPacks), done];
-        if (type === 3) return [Math.max(...subPacks), done];
-        if (type === 5) return s1 > s2 ? [1, done] : [0, done];
-        if (type === 6) return s1 < s2 ? [1, done] : [0, done];
-        if (type === 7) return s1 === s2 ? [1, done] : [0, done];
+function literal(reader, bitCnt, readXtra) {
+    // literal packet, read values
+    let values = [];
+    let last = false;
+    while (!last) {
+        last = reader.next(1).value === '0';
+        values.push(reader.next(4).value);
+        bitCnt += 5;
     }
+    let xtraBits = readXtra ? 4 - (bitCnt % 4) : 0;
+    let { done } = reader.next(xtraBits);
+    return [b2i(values.join('')), done];
 }
 
 function getSubPacks(reader, bitCnt, readXtra) {
@@ -69,7 +53,7 @@ function getSubPacks(reader, bitCnt, readXtra) {
         while (more) {
             let [subPack, done] = readPacket(subReader, false);
             subPacks.push(subPack);
-            more = !done;
+            more = !subReader.next(0).done;
         }
     }
     if (lengthTypeId === '1') {
@@ -84,6 +68,26 @@ function getSubPacks(reader, bitCnt, readXtra) {
     let xtraBits = readXtra ? 4 - (bitCnt % 4) : 0;
     let { done } = reader.next(xtraBits);
     return [subPacks, done];
+}
+
+function readPacket(reader, readXtra = true) {
+    versions.push(reader.next(3).value);
+    let type = b2i(reader.next(3).value);
+    let bitCnt = 6;
+
+    // literal packet?
+    if (type === 4) return literal(reader, bitCnt, readXtra);
+
+    // operator packet
+    let [subPacks, done] = getSubPacks(reader, bitCnt, readXtra);
+    let [s1, s2] = subPacks;
+    if (type === 0) return [sum(subPacks), done];
+    if (type === 1) return [product(subPacks), done];
+    if (type === 2) return [Math.min(...subPacks), done];
+    if (type === 3) return [Math.max(...subPacks), done];
+    if (type === 5) return s1 > s2 ? [1, done] : [0, done];
+    if (type === 6) return s1 < s2 ? [1, done] : [0, done];
+    if (type === 7) return s1 === s2 ? [1, done] : [0, done];
 }
 
 function main() {

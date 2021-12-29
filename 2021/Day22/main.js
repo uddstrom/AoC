a480a287b09689eb3501009bbea2c1e0ce810290
Jesper Uddström <jesper@uddstrom.se>
2021-12-29 16:52:47 +0100

    wip

--- 2021/Day22/main.js
+++ 2021/Day22/main.js
@@ -15,30 +15,6 @@ var parser = (input) => {
     return instructions;
 };
 
-// function init(instructions) {
-//     let count = new Set();
-//     let instr = instructions.slice(0, 20).map((i) => {
-//         return {
-//             state: i.state,
-//             x: range(i.coords[0][0], i.coords[0][1]),
-//             y: range(i.coords[1][0], i.coords[1][1]),
-//             z: range(i.coords[2][0], i.coords[2][1]),
-//         };
-//     });
-//     instr.forEach((i) => {
-//         i.x.forEach((x) => {
-//             i.y.forEach((y) => {
-//                 i.z.forEach((z) => {
-//                     i.state
-//                         ? count.add(`${x},${y},${z}`)
-//                         : count.delete(`${x},${y},${z}`);
-//                 });
-//             });
-//         });
-//     });
-//     return count.size;
-// }
-
 function init(instructions) {
     return reboot(instructions.slice(0, 20));
 }
@@ -57,46 +33,6 @@ function reboot(instructions) {
     let Y = new Set();
     let Z = new Set();
 
-    function split(i) {
-        let xmin = i.coords[0][0];
-        let xmax = i.coords[0][1];
-        let ymin = i.coords[1][0];
-        let ymax = i.coords[1][1];
-        let zmin = i.coords[2][0];
-        let zmax = i.coords[2][1];
-
-        let cubes = X.filter((x) => x >= xmin)
-            .filter((x) => x <= xmax)
-            .flatMap((x, ix, xarr) =>
-                Y.filter((y) => y >= ymin)
-                    .filter((y) => y <= ymax)
-                    .flatMap((y, iy, yarr) =>
-                        Z.filter((z) => z >= zmin)
-                            .filter((z) => z <= zmax)
-                            .flatMap((z, iz, zarr) => {
-                                let coords = matrix(3, 2);
-                                coords[0][0] = x;
-                                coords[0][1] =
-                                    xarr[ix + 1] !== undefined
-                                        ? xarr[ix + 1] - 1
-                                        : xmax;
-                                coords[1][0] = y;
-                                coords[1][1] =
-                                    yarr[iy + 1] !== undefined
-                                        ? yarr[iy + 1] - 1
-                                        : ymax;
-                                coords[2][0] = z;
-                                coords[2][1] =
-                                    zarr[iz + 1] !== undefined
-                                        ? zarr[iz + 1] - 1
-                                        : zmax;
-                                return { state: i.state, coords };
-                            })
-                    )
-            );
-        return cubes;
-    }
-
     instructions.forEach((i) => {
         X.add(i.coords[0][0]);
         X.add(i.coords[0][1] + 1);
@@ -110,17 +46,53 @@ function reboot(instructions) {
     Y = Array.from(Y).sort((a, b) => a - b);
     Z = Array.from(Z).sort((a, b) => a - b);
 
-    let cuboids = [];
-    cuboids = instructions.map(split);
-
-    let id = (cuboid) =>
-        `${cuboid.coords[0][0]},${cuboid.coords[0][1]},${cuboid.coords[1][0]},${cuboid.coords[1][1]},${cuboid.coords[2][0]},${cuboid.coords[2][1]}`;
     let count = new Set();
 
-    cuboids.forEach((cuboid) => {
-        cuboid.forEach((subC) => {
-            subC.state ? count.add(id(subC)) : count.delete(id(subC));
-        });
+    function id(cuboid) {
+        return `${cuboid.coords[0][0]},${cuboid.coords[0][1]},${cuboid.coords[1][0]},${cuboid.coords[1][1]},${cuboid.coords[2][0]},${cuboid.coords[2][1]}`;
+    }
+
+    function split(i) {
+        let xmin = i.coords[0][0];
+        let xmax = i.coords[0][1];
+        let ymin = i.coords[1][0];
+        let ymax = i.coords[1][1];
+        let zmin = i.coords[2][0];
+        let zmax = i.coords[2][1];
+
+        // let cubes = new Set();
+        X.filter((x) => x >= xmin && x <= xmax).forEach((x, ix, xarr) =>
+            Y.filter((y) => y >= ymin && y <= ymax).forEach((y, iy, yarr) =>
+                Z.filter((z) => z >= zmin && z <= zmax).forEach(
+                    (z, iz, zarr) => {
+                        let coords = matrix(3, 2);
+                        coords[0][0] = x;
+                        coords[0][1] =
+                            xarr[ix + 1] !== undefined
+                                ? xarr[ix + 1] - 1
+                                : xmax;
+                        coords[1][0] = y;
+                        coords[1][1] =
+                            yarr[iy + 1] !== undefined
+                                ? yarr[iy + 1] - 1
+                                : ymax;
+                        coords[2][0] = z;
+                        coords[2][1] =
+                            zarr[iz + 1] !== undefined
+                                ? zarr[iz + 1] - 1
+                                : zmax;
+                        i.state
+                            ? count.add(id({ coords }))
+                            : count.delete(id({ coords }));
+                    }
+                )
+            )
+        );
+    }
+
+    instructions.forEach((i, index) => {
+        split(i);
+        console.log(`processed ${index + 1} of ${instructions.length}`);
     });
 
     return [...count].reduce((acc, curr) => acc + volume(curr), 0);

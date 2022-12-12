456d954be72f5f3f16eab71fa1dc5154ab3832f6
Uddström, Jesper (Consultant) <Jesper.Uddstrom@skandia.se>
2022-12-12 18:27:29 +0100

    day 10

--- 2022/Day10/main.js
+++ 2022/Day10/main.js
@@ -10,44 +10,41 @@ function parser(input) {
 }
 
 function draw(CRT) {
-    var rows = Array(6)
-        .fill()
-        .map((_) => '');
-    for (let i = 0; i < CRT.length; i++) {
-        let row = Math.floor(i / 40);
-        rows[row] = rows[row].concat(CRT[i]);
-    }
-    console.log(rows);
+    console.log(CRT.slice(0,40).join(''));
+    console.log(CRT.slice(40,80).join(''));
+    console.log(CRT.slice(80,120).join(''));
+    console.log(CRT.slice(120,160).join(''));
+    console.log(CRT.slice(160,200).join(''));
+    console.log(CRT.slice(200,240).join(''));
 }
-
-// 20th, 60th, 100th, 140th, 180th, and 220th cycles
 
 var data = getData(PUZZLE_INPUT_PATH)(parser); // [1, 2, NaN, 3, 4, ...]
 
 var measurPoints = [20, 60, 100, 140, 180, 220];
 var measures = [];
-var cycle = 1;
+var cycle = 0;
 var x = 1;
 var adding = false;
-var CRT = rng(6 * 40).map((_) => '.');
-
+var CRT = rng(6 * 40).map((_) => ' ');
+var val;
 while (data.length > 0) {
-    if (measurPoints.includes(cycle)) {
-        measures.push(x * cycle);
+    if (measurPoints.includes(cycle+1)) {
+        measures.push(x * (cycle+1));
+    }
+    var spritePos = (cycle) % 40;
+    if (spritePos >= x-1 && spritePos <= x+1) {
+        CRT[cycle] = '#';
     }
     if (adding) {
         x += val;
         adding = false;
     } else {
-        var val = data.shift();
-        if (Number.isNaN(val)) {
-            /* noop, continue */
-        } else {
+        val = data.shift();
+        if (!Number.isNaN(val)) 
             adding = true;
-        }
     }
     cycle++;
 }
 
 console.log('Part 1:', sum(measures));
-console.log('Part 2:', draw(CRT));
+console.log('Part 2:'); draw(CRT);

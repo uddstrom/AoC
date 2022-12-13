1105afae2c368d6521253f685c476c12aa5f1b1c
Jesper Uddström <jesper@uddstrom.se>
2022-12-13 23:24:33 +0100

    day 11

--- 2022/Day11/main.js
+++ 2022/Day11/main.js
@@ -1,71 +1,66 @@
-import { getData, getPath } from '../lib/utils.js';
+import { getData, getPath, product } from '../lib/utils.js';
 
 var PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/puzzle_input`;
 
 function parser(input) {
-    var monkeys = new Map();
-    input
-        .split('\n\n')
-        .map(parseMonkey)
-        .forEach((monkey) => {
-            monkeys.set(monkey.id, monkey);
-        });
-    return monkeys;
+    return input.split('\n\n').map(parseMonkey);
 }
 
 function parseMonkey(monkeyString) {
-    var grabMonkeyNumber = (monkeyRow) => Number(monkeyRow.match(/(\d+)/g)[0]);
+    var grabMonkeyNumber = (monkeyRow) => BigInt(monkeyRow.match(/(\d+)/g)[0]);
     var monkeyRows = monkeyString.split('\n');
-    var opComponents = monkeyRows[2].trim().split(' ');
+    var [val, operator, ..._] = monkeyRows[2].split(' ').reverse();
     return {
         id: grabMonkeyNumber(monkeyRows[0]),
-        items: monkeyRows[1].match(/(\d{2})/g).map(Number),
-        op: {
-            operator: opComponents[4],
-            value: Number(opComponents[5]),
+        items: monkeyRows[1].match(/(\d{2})/g).map(BigInt),
+        operation: {
+            operator,
+            value: val === 'old' ? null : BigInt(val),
         },
         testValue: grabMonkeyNumber(monkeyRows[3]),
         ifTrue: grabMonkeyNumber(monkeyRows[4]),
         ifFalse: grabMonkeyNumber(monkeyRows[5]),
-        inspections: 0n
+        inspections: 0,
     };
 }
 
-function calculateMonkeyBusinessLevel(monkeys, rounds, inspectFn) {
-    var test = (item, testValue) => item % testValue === 0;
-    var throwToMonkey = (item, monkeyId) => monkeys.get(monkeyId).items.push(item);
-    
-    for (let i = 0; i < rounds; i++) {
-        for (let [_, monkey] of monkeys) {
-            while (monkey.items.length > 0) {
-                monkey.inspections++;
-                let inspectedItem = inspectFn(monkey.items.shift(), monkey.op);
-                test(inspectedItem, monkey.testValue)
-                    ? throwToMonkey(inspectedItem, monkey.ifTrue)
-                    : throwToMonkey(inspectedItem, monkey.ifFalse);
-            }
-        }
-    }
-    
-    var [m1, m2, ..._] = Array
-                            .from(monkeys, ([_, value]) => value)
-                            .sort((m1, m2) => {
-                                if (m1.inspections > m2.inspections) return -1;
-                                if (m1.inspections < m2.inspections) return 1;
-                                return 0;
-                            });
+function calculateMonkeyBusinessLevel(_monkeys, rounds, inspect) {
+    var monkeys = structuredClone(_monkeys);
+    var lcd = product(monkeys.map((m) => m.testValue));
+    var test = (item, testValue) => item % testValue === 0n;
+    var throwTo = (item, id) =>
+        monkeys.find((m) => m.id === id).items.push(item);
 
+    for (let i = 0; i < rounds; i++) {
+        monkeys.forEach((monkey) => {
+            monkey.items = monkey.items.map((i) => i % lcd);
+            monkey.inspections += monkey.items.length;
+            while (monkey.items.length > 0) {
+                let inspectedItem = inspect(
+                    monkey.items.shift(),
+                    monkey.operation
+                );
+                test(inspectedItem, monkey.testValue)
+                    ? throwTo(inspectedItem, monkey.ifTrue)
+                    : throwTo(inspectedItem, monkey.ifFalse);
+            }
+        });
+    }
+
+    var [m1, m2, ..._] = monkeys.sort((a, b) => b.inspections - a.inspections);
     return m1.inspections * m2.inspections;
 }
 
 var data = getData(PUZZLE_INPUT_PATH)(parser);
 
 var worryFuncs = {
-    '+': (item, value) => Number.isNaN(value) ? item + item : item + value,
-    '*': (item, value) => Number.isNaN(value) ? item * item : item * value,
+    '+': (item, value) => (value === null ? item + item : item + value),
+    '*': (item, value) => (value === null ? item * item : item * value),
 };
-var p1Inspect = (item, { operator, value }) => Math.floor(worryFuncs[operator](item, value) / 3);
-var p2Inspect = (item, { operator, value }) => worryFuncs[operator](item, value);
+var p1Inspect = (item, { operator, value }) =>
+    worryFuncs[operator](item, value) / 3n;
+var p2Inspect = (item, { operator, value }) =>
+    worryFuncs[operator](item, value);
 
 console.log('Part 1:', calculateMonkeyBusinessLevel(data, 20, p1Inspect));
 console.log('Part 2:', calculateMonkeyBusinessLevel(data, 10000, p2Inspect));

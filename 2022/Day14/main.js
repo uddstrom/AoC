f8c1149277e36cc99f54c6fee5da2a23921e4d6d
Jesper Uddström <jesper@uddstrom.se>
2022-12-18 23:41:41 +0100

    refactor day 14

--- 2022/Day14/main.js
+++ 2022/Day14/main.js
@@ -1,9 +1,7 @@
-import { count, getData, getPath, matrix, min, max } from '../lib/utils.js';
-import { not } from '../lib/fn.js';
+import { getData, getPath, matrix, max, min, range } from '../lib/utils.js';
+import { trampoline } from '../lib/fn.js';
 
 var PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/puzzle_input`;
-
-var sandsource;
 
 function parser(input) {
     var scan = input
@@ -12,78 +10,75 @@ function parser(input) {
             row.split(' -> ').map((cord) => cord.split(',').map(Number))
         );
 
-    /* scan looks like this:
-    [
-        [ [ 498, 4 ], [ 498, 6 ], [ 496, 6 ] ],
-        [ [ 503, 4 ], [ 502, 4 ], [ 502, 9 ], [ 494, 9 ] ]
-    ]
-    */
-
     var maxR = max(scan.map((line) => line.map(([c, r]) => r)).flat());
-    var minC = min(scan.map((line) => line.map(([c, r]) => c)).flat());
     var maxC = max(scan.map((line) => line.map(([c, r]) => c)).flat());
-    var map = matrix(maxR + 1, maxC - minC + 1, '.');
-    console.log(`${map.length}x${map[0].length}`);
+    var map = matrix(maxR + 3, maxC, '.');
 
-    var translate = ([r, c]) => [r, c - minC];
     scan.map((line) => lineToCoords(line))
         .flat()
-        .map(translate)
         .forEach(([row, col]) => (map[row][col] = '#'));
 
-    sandsource = translate([0, 500]);
-    var [sr, sc] = sandsource;
-    map[sr][sc] = '+'; // Sand source
+    map[map.length - 1] = range(0, map[0].length).map((_) => '#'); // Floor
+    map[0][500] = '+'; // Sand source
     return map;
 }
 
 function lineToCoords(line, coords = []) {
     // line: [ [ 498, 4 ], [ 498, 6 ], [ 496, 6 ] ]
     if (line.length < 2) return coords;
-    var [head, next, ...tail] = line;
-    var [start_c, start_r] = head;
-    var [end_c, end_r] = next;
+    var [[start_c, start_r], [end_c, end_r], ...tail] = line;
     var from_r = min([start_r, end_r]);
     var to_r = max([start_r, end_r]);
     var from_c = min([start_c, end_c]);
     var to_c = max([start_c, end_c]);
-    for (let r = from_r; r <= to_r; r++) {
-        for (let c = from_c; c <= to_c; c++) {
-            coords.push([r, c]);
-        }
-    }
-    return lineToCoords([next, ...tail], coords);
+    range(from_r, to_r).forEach((r) => {
+        range(from_c, to_c).forEach((c) => coords.push([r, c]));
+    });
+    return lineToCoords([[end_c, end_r], ...tail], coords);
 }
 
-function render(matrix) {
-    matrix.map((row) => row.join('')).forEach((row) => console.log(row));
-}
+function dropSand(map, source) {
+    var [r, c] = source ? source : [0, map[0].indexOf('+')];
+    var notInMap = ([r, c]) =>
+        r < 0 || r >= map.length ? true : c < 0 || c >= map[r].length;
+    var notBlocked = ([r, c]) => notInMap([r, c]) || map[r][c] === '.';
 
-function dropSand(map, [r, c]) {
-    var inMap = ([r, c]) => r >= 0 && r < map.length ? c >= 0 && c < map[r].length : false;
-    var blocked = ([r, c]) => inMap([r, c]) && (map[r][c] === '#' || map[r][c] === 'o');
-    // down, downleft, downright
-    var next = [[r + 1, c], [r + 1, c - 1], [r + 1, c + 1]].find(not(blocked));
-    if (next === undefined) {
+    var next = [
+        [r + 1, c], // down
+        [r + 1, c - 1], // downleft
+        [r + 1, c + 1], // downright
+    ].find(notBlocked);
+    if (r === map.length - 2 || next === undefined) {
         // at rest, update map
         map[r][c] = 'o';
         return map;
     }
-    if (not(inMap)(next)) return;
+    if (notInMap(next)) {
+        next = extend(map, next);
+    }
     return dropSand(map, next);
 }
 
-
-var map = getData(PUZZLE_INPUT_PATH)(parser);
-
-var mapFullWithSand = structuredClone(map);
-while (true) {
-    var m = dropSand(mapFullWithSand, sandsource);
-    if(m) mapFullWithSand = m;
-    else break;
+function extend(map, next) {
+    var [nr, nc] = next;
+    if (nc >= map[0].length) {
+        map.forEach((row) => row.push('.'));
+    } else if (nc < 0) {
+        map.forEach((row) => row.unshift('.'));
+        next = [nr, 0];
+    }
+    map[map.length - 1].map((_) => '#');
+    return next;
 }
 
-render(mapFullWithSand);
+function runSimulation(map, p1 = 0, p2 = 0) {
+    if (!map[0].includes('+')) return [p1, p2];
+    map = dropSand(map);
+    if (!map[map.length - 2].includes('o')) p1++;
+    return () => runSimulation(map, p1, p2 + 1);
+}
 
-console.log('Part 1:', count('o', mapFullWithSand.flat()));
-// console.log('Part 2:');
+var map = getData(PUZZLE_INPUT_PATH)(parser);
+var [p1, p2] = trampoline(runSimulation)(map);
+console.log('Part 1:', p1);
+console.log('Part 2:', p2);

f44f83ca6a6f1ca1eb3a4e81f61ff1efee27f169
Jesper Uddström <jesper@uddstrom.se>
2022-12-25 21:47:36 +0100

    day 16 part 1

--- 2022/Day16/main.js
+++ 2022/Day16/main.js
@@ -1,55 +1,106 @@
 import { getData, getPath } from '../lib/utils.js';
-import { createGraph } from './graph.js';
+import { PrioQ } from './PrioQ.js';
 
 var PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/puzzle_input`;
 
 function parser(input) {
-    return input.split('\n').map((row) => {
+    var nodes = new Map();
+    input.split('\n').forEach((row) => {
         let captures = row
             .match(
                 /Valve (\w{2}) has flow rate=(\d+); tunnel[s]? lead[s]? to valve[s]? (.+)/
             )
             .slice(1);
-        return {
+
+        nodes.set(captures[0], {
             id: captures[0],
             flowRate: Number(captures[1]),
             edgesTo: captures[2].split(', '),
-            isOpen: false,
-        };
+        });
     });
+    return nodes;
 }
 
-function printPath(path) {
-    if (path.previous) {
-        printPath(path.previous);
+function dijk(nodes, start, goal) {
+    var visited = new PrioQ();
+    var openSet = new PrioQ();
+    var maxRelease = 0;
+
+    openSet.push(start);
+
+    while (!openSet.empty()) {
+        var current = openSet.pop();
+        visited.push(current);
+
+        if (current.time === goal.time) {
+            if (current.totalRelease > maxRelease) {
+                maxRelease = current.totalRelease;
+            }
+        } else {
+            // construct new state
+            // three options:
+            let currentFlowRate = nodes.get(current.location.id).flowRate;
+            if (!current.location.isOpen && currentFlowRate > 0) {
+                // 2. valve not and has flowRate. open valve
+                var newState = {
+                    time: current.time + 1,
+                    location: {
+                        id: current.location.id,
+                        isOpen: true,
+                    },
+                    flowRate: current.flowRate + currentFlowRate,
+                    totalRelease: current.totalRelease + current.flowRate,
+                    previous: current,
+                };
+                openSet.push(newState);
+            }
+            // 3. move to next location
+            // explore edges
+            var edges = nodes.get(current.location.id).edgesTo;
+            edges.forEach((edge) => {
+                var newState = {
+                    time: current.time + 1,
+                    location: {
+                        id: edge,
+                        isOpen: hasBeenOpened(edge, current),
+                    },
+                    flowRate: current.flowRate,
+                    totalRelease: current.totalRelease + current.flowRate,
+                    previous: current,
+                };
+
+                // ignore new state if visited already
+                if (!visited.get(newState)) {
+                    openSet.push(newState);
+                }
+            });
+        }
     }
-    console.log(
-        `[${path.time},${path.location.id},${
-            path.location.isOpen ? 'open' : 'closed'
-        },${path.flowRate},${path.totalRelease}]`
-    );
+
+    return maxRelease;
+}
+
+function hasBeenOpened(id, current) {
+    if (current.location.id === id && current.location.isOpen) return true;
+    if (!current.previous) return false;
+    return hasBeenOpened(id, current.previous);
 }
 
 var nodes = getData(PUZZLE_INPUT_PATH)(parser);
 
-var graph = createGraph(nodes);
-graph.display();
-
 var start = {
     time: 0,
-    location: graph.nodes.find((n) => n.id === 'AA'),
+    location: { id: 'AA', isOpen: false },
     flowRate: 0,
     totalRelease: 0,
 };
+
+var startTime = Date.now();
 var goal = { time: 30 };
-var paths = graph
-    .dijk(start, goal)
-    .sort((p1, p2) => p2.totalRelease - p1.totalRelease)
-    .slice(0, 3);
+var maxRelease = dijk(nodes, start, goal);
+var endTime = Date.now();
+var time = (endTime - startTime) / 1000;
 
-// paths.forEach(printPath);
+console.log('Part 1:', maxRelease, time);
 
-console.log('Part 1:', paths[0].totalRelease);
 console.log('Part 2:');
-
-// > 1563

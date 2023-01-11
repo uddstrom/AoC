db57b55cc7263a44e1528de45a84cbf271aa487f
Uddström, Jesper (Consultant) <Jesper.Uddstrom@skandia.se>
2023-01-11 09:06:49 +0100

    Day 20

--- 2022/Day20/main.js
+++ 2022/Day20/main.js
@@ -3,81 +3,44 @@ import { getData, getPath, sum } from '../lib/utils.js';
 var PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/puzzle_input`;
 
 function parser(input) {
-    return input.split('\n').map((n, i) => {
-        return [i, Number(n)]; // [position, value]
-    });
+    return input.split('\n').map((n, i) => [i, Number(n)]); // [position, value]
+}
+
+function mixing(data, rounds) {
+    var L = data.length - 1;
+    for (let index = 0; index < rounds; index++) {
+        data.forEach(([pos, val], idx) => {
+            let steps = val % L;
+            if (steps < 0) steps += L;
+    
+            let newPos = pos + steps;
+            if (newPos > L - 1) newPos -= L;
+
+            data[idx] = [newPos, val];
+            data.forEach(([p, v], i) => {
+                if (i !== idx && p > pos && p <= newPos) {
+                    data[i] = [p - 1, v];
+                }
+                if (i !== idx && p < pos && p >= newPos) {
+                    data[i] = [p + 1, v];
+                }
+            });
+        });
+    }
+
+    var [indexOfZero, _] = data.find(([p, v]) => v === 0);
+    
+    return sum(
+        [1000, 2000, 3000].map((nth) => {
+            let i = (nth + indexOfZero) % L;
+            let [_, val] = data.find(([p, v]) => p === i);
+            return val;
+        })
+    );
 }
 
 var p1_data = getData(PUZZLE_INPUT_PATH)(parser);
 var p2_data = p1_data.map(([pos, val]) => [pos, val * 811589153]);
 
-var L = p1_data.length;
-
-p1_data.forEach(([pos, val], idx) => {
-    let steps = val;
-    while (steps < 0) {
-        steps += L - 1;
-    }
-
-    let newPos = pos + steps;
-    while (newPos > L - 1) {
-        newPos -= L - 1;
-    }
-
-    p1_data[idx] = [newPos, val];
-    p1_data.forEach(([p, v], i) => {
-        if (i !== idx && p > pos && p <= newPos) {
-            p1_data[i] = [p - 1, v];
-        }
-        if (i !== idx && p < pos && p >= newPos) {
-            p1_data[i] = [p + 1, v];
-        }
-    });
-});
-
-var [indexOfZero, _] = p1_data.find(([p, v]) => v === 0);
-var p1 = sum(
-    [1000, 2000, 3000].map((nth) => {
-        let i = (nth + indexOfZero) % L;
-        let [_, val] = p1_data.find(([p, v]) => p === i);
-        return val;
-    })
-);
-
-console.log('Part 1:', p1);
-
-for (let index = 0; index < 10; index++) {
-    console.log('round', index);
-    p2_data.forEach(([pos, val], idx) => {
-        let steps = val;
-        while (steps < 0) {
-            steps += L - 1;
-        }
-
-        let newPos = pos + steps;
-        while (newPos > L - 1) {
-            newPos -= L - 1;
-        }
-
-        p2_data[idx] = [newPos, val];
-        p2_data.forEach(([p, v], i) => {
-            if (i !== idx && p > pos && p <= newPos) {
-                p2_data[i] = [p - 1, v];
-            }
-            if (i !== idx && p < pos && p >= newPos) {
-                p2_data[i] = [p + 1, v];
-            }
-        });
-    });
-}
-
-var [indexOfZero, _] = p2_data.find(([p, v]) => v === 0);
-var p2 = sum(
-    [1000, 2000, 3000].map((nth) => {
-        let i = (nth + indexOfZero) % L;
-        let [_, val] = p2_data.find(([p, v]) => p === i);
-        return val;
-    })
-);
-
-console.log('Part 2:', p2);
+console.log('Part 1:', mixing(p1_data, 1));
+console.log('Part 2:', mixing(p2_data, 10));

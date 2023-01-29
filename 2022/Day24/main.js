436652088445aff7f4163ca474da9169cedede4a
Jesper Uddström <jesper@uddstrom.se>
2023-01-29 23:21:23 +0100

    wip

--- 2022/Day24/main.js
+++ 2022/Day24/main.js
@@ -1,5 +1,5 @@
 import { getData, getPath, rng } from '../lib/utils.js';
-import { PrioQ } from '../lib/aStar.js';
+import { PrioQ } from './PrioQ.js';
 import { makeGrid } from '../lib/grid.js';
 
 var PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/puzzle_input`;
@@ -13,12 +13,13 @@ function parser(input) {
         ROWS = rows.length;
         row.split('').forEach((col, c, cols) => {
             COLS = cols.length;
-            if (r === 0 && col === '.') start = [r, c];
-            if (r === rows.length - 1 && col === '.') goal = [r, c];
+            if (r === 0 && col === '.') start = { r, c };
+            if (r === rows.length - 1 && col === '.') goal = { r, c };
             if (isBlizzard(col)) blizzards.add({ r, c, dir: col });
         });
     });
-    return [start, goal, blizzards];
+    start.blizzards = blizzards;
+    return [start, goal];
 }
 
 function show([start_r, start_c], [goal_r, goal_c], blizzards) {
@@ -57,18 +58,69 @@ function simulate(B) {
     return B2;
 }
 
-function aStar(start, goal, blizzards) {
-    console.log('INITIAL STATE');
-    show(start, goal, blizzards);
-    rng(5).forEach((n) => {
-        blizzards = simulate(blizzards);
-        console.log('RUNDA ', n + 1);
-        show(start, goal, blizzards);
+function getNeighbors(current, blizzards, start, goal) {
+    var candidates = DIRS.map(([dr, dc]) => ({
+        r: current.r + dr,
+        c: current.c + dc,
+        g: Number.MAX_SAFE_INTEGER,
+    }));
+    candidates.push({
+        r: current.r,
+        c: current.c,
+        g: Number.MAX_SAFE_INTEGER,
     });
+    candidates = candidates.filter(({ r, c }) => {
+        if (r === start.r && c === start.c) return true;
+        if (r === goal.r && c === goal.c) return true;
+        if (r <= 0) return false;
+        if (r >= ROWS - 1) return false;
+        if (c <= 0) return false;
+        if (c >= COLS - 1) return false;
+        if ([...blizzards].some((b) => r === b.r && c === b.c)) return false;
+        return true;
+    });
+    return candidates;
 }
 
-var [start, goal, blizzards] = getData(PUZZLE_INPUT_PATH)(parser);
+function aStar(start, goal) {
+    var compareFn = (n1, n2) => n1.f - n2.f;
+    var h = (n1, n2) => Math.abs(n1.r - n2.r) + Math.abs(n1.c - n2.c);
+    var openSet = new PrioQ(compareFn);
+
+    openSet.push(start);
+    start.g = 0;
+    start.f = h(start, goal);
+
+    while (!openSet.empty()) {
+        var current = openSet.pop();
+        console.log(current.g, openSet.queue.length, openSet.ids.size);
+        if (current.r === goal.r && current.c === goal.c) {
+            return current.f;
+        }
+
+        var blizzards = simulate(current.blizzards);
+        current.neighbors = getNeighbors(current, blizzards, start, goal);
+
+        current.neighbors.forEach((neighbor) => {
+            // tentative_gScore is the distance from start to the neighbor through current
+            var tentative_gScore = current.g + 1;
+
+            if (tentative_gScore < neighbor.g) {
+                // This path to neighbor is better than any previous one. Record it!
+                neighbor.blizzards = blizzards;
+                neighbor.g = tentative_gScore;
+                neighbor.f = tentative_gScore + h(neighbor, goal);
+
+                if (!openSet.includes(neighbor)) {
+                    openSet.push(neighbor);
+                }
+            }
+        });
+    }
+}
+
+var [start, goal] = getData(PUZZLE_INPUT_PATH)(parser);
 var startTime = Date.now();
-console.log('Part 1:', aStar(start, goal, blizzards));
+console.log('Part 1:', aStar(start, goal));
 console.log('Part 2:');
 console.log('Completed in (ms):', Date.now() - startTime);

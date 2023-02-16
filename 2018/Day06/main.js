6d1d8c9ff9248f18585061cb6e5a5771202ee04a
Jesper <jesper@uddstrom.se>
2023-02-16 20:59:33 +0100

    Day 6

--- 2018/Day06/main.js
+++ 2018/Day06/main.js
@@ -3,11 +3,54 @@ import { count, getData, getPath, min, max, range } from '../lib/utils.js';
 var PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/puzzle_input`;
 
 function parser(input) {
-    return input.split('\n').map((coords) => coords.split(', ').map(Number));
+	return input.split('\n').map((coords) => coords.split(', ').map(Number));
 }
 
 function manhattan([x1, y1], [x2, y2]) {
 	return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+}
+
+function isFinite([x, y]) {
+
+	var coordsBelow = coords.filter(([_, yy]) => yy > y);
+	var coordsAbove = coords.filter(([_, yy]) => yy < y);
+	var coordsLeft = coords.filter(([xx, _]) => xx < x);
+	var coordsRight = coords.filter(([xx, _]) => xx > x);
+
+	var stopperBelow = false;
+	var stopperAbove = false;
+	var stopperRight = false;
+	var stopperLeft = false;
+
+	for (let [xx, yy] of coordsBelow) {
+		if (Math.abs(yy - y) >= Math.abs(xx - x)) {
+			stopperBelow = true;
+			break;
+		}
+	}
+
+	for (let [xx, yy] of coordsAbove) {
+		if (Math.abs(yy - y) >= Math.abs(xx - x)) {
+			stopperAbove = true;
+			break;
+		}
+	}
+
+	for (let [xx, yy] of coordsLeft) {
+		if (Math.abs(xx - x) >= Math.abs(yy - y)) {
+			stopperLeft = true;
+			break;
+		}
+	}
+
+	for (let [xx, yy] of coordsRight) {
+		if (Math.abs(yy - y) >= Math.abs(xx - x)) {
+			stopperRight = true;
+			break;
+		}
+	}
+
+	return stopperBelow && stopperAbove && stopperRight && stopperLeft;
 }
 
 var coords = getData(PUZZLE_INPUT_PATH)(parser);
@@ -18,7 +61,7 @@ var miny = min(coords.map(([x, y]) => y));
 var maxy = max(coords.map(([x, y]) => y));
 
 function p1() {
-	// the grid contains the index of the clostest coord.
+	// the grid contains the index of the closest coord.
 	var grid = range(miny, maxy).map(y => {
 		return range(minx, maxx).map(x => {
 			var min_d = Number.MAX_SAFE_INTEGER;
@@ -36,65 +79,17 @@ function p1() {
 			});
 			return tie ? -1 : min_i;
 		});
-	});
+	}).flat();
 
-	var non_infinite = coords.filter(([x, y]) => {
-		
-		var isBelow = coords.filter(([xx, yy]) => yy > y);
-		var isAbove = coords.filter(([xx, yy]) => yy < y);
-		var isLeft = coords.filter(([xx, yy]) => xx < x);
-		var isRight = coords.filter(([xx, yy]) => xx > x);
-		
-		var stopperBelow = false;
-		var stopperAbove = false;
-		var stopperRight = false;
-		var stopperLeft = false;
+	var non_infinite = coords.filter(isFinite);
 
-		for (let [xx, yy] of isBelow) {
-			if (Math.abs(yy - y) >= Math.abs(xx - x)) {
-				stopperBelow = true;
-				break;
-			}
-		}
-		
-		for (let [xx, yy] of isAbove) {
-			if (Math.abs(yy - y) >= Math.abs(xx - x)) {
-				stopperAbove = true;
-				break;
-			}
-		}
-		
-		for (let [xx, yy] of isLeft) {
-			if (Math.abs(xx - x) >= Math.abs(yy - y)) {
-				stopperLeft = true;
-				break;
-			}
-		}
-		
-		for (let [xx, yy] of isRight) {
-			if (Math.abs(yy - y) >= Math.abs(xx - x)) {
-				stopperRight = true;
-				break;
-			}
-		}
-
-		return stopperBelow && stopperAbove && stopperRight && stopperLeft;
-
-	});
-
-	return max(coords.map((c, i) => {
-		if (non_infinite.includes(c)) {
-			return count(i, grid.flat());
-		}
-		return 0;
-	}));
+	return max(coords.map((c, i) => non_infinite.includes(c) ? count(i, grid) : 0));
 }
 
-
-function p2(limit) {
+function p2() {
 	var grid = range(miny, maxy).map(y => {
 		return range(minx, maxx).map(x => {
-			return coords.reduce((acc, curr) => acc + manhattan(curr, [x, y]), 0) < limit
+			return coords.reduce((acc, curr) => acc + manhattan(curr, [x, y]), 0) < 10000
 				? 1 : 0;
 		});
 	});
@@ -102,4 +97,4 @@ function p2(limit) {
 }
 
 console.log('Part 1:', p1());
-console.log('Part 2:', p2(10000));
+console.log('Part 2:', p2());

20e4328f6ce15e807cb963efdf0b5bbda4ffe86d
Jesper Uddström <jesper@uddstrom.se>
2023-10-15 19:20:31 +0200

    solve part 2

--- 2019/Day20/main.js
+++ 2019/Day20/main.js
@@ -25,7 +25,12 @@ function getNeighbours([r, c], grid) {
     return neighbors;
 }
 
-function findPortal(portal, grid) {
+function isOuterPortal(r, c) {
+    var isouter = r === 2 || r === ROWS - 3 || c === 2 || c === COLS - 3;
+    return isouter;
+}
+
+function findPortal(portal, grid, recursive = false) {
     var [p1, p2] = portal.split("");
     var portals = grid
         .map((row, r1) => {
@@ -35,67 +40,75 @@ function findPortal(portal, grid) {
                     if (nOfP1.includes(p2)) {
                         if (nOfP1.includes(".")) {
                             let dir = nOfP1.indexOf(".");
-                            return [r1 + DR[dir], c1 + DC[dir]];
+                            let dl = recursive ? (isOuterPortal(r1 + DR[dir], c1 + DC[dir]) ? -1 : 1) : 0;
+                            return [r1 + DR[dir], c1 + DC[dir], dl];
                         }
                         let dirOfP2 = nOfP1.indexOf(p2);
                         let r2 = r1 + DR[dirOfP2];
                         let c2 = c1 + DC[dirOfP2];
                         let nOfP2 = getNeighbours([r2, c2], grid);
                         let dir = nOfP2.indexOf(".");
-                        return [r2 + DR[dir], c2 + DC[dir]];
+                        let dl = recursive ? (isOuterPortal(r2 + DR[dir], c2 + DC[dir]) ? -1 : 1) : 0;
+                        return [r2 + DR[dir], c2 + DC[dir], dl];
                     }
                 }
             }
         })
         .filter((x) => x);
     // remove duplicates
-    var S = new Set(portals.map(([r, c]) => `${r},${c}`));
+    var S = new Set(portals.map(([r, c, l]) => `${r},${c},${l}`));
     return Array.from(S).map((x) => x.split(",").map(Number));
 }
 
-function getAdjacent([r, c], grid) {
-    var adj = []; // coords of neighbours that can be reached
-    var neighbors = getNeighbours([r, c], grid);
-    neighbors.forEach((n, dir) => {
-        if (n === ".") adj.push([r + DR[dir], c + DC[dir]]);
-        if (n && n.match(/^[A-Z]$/)) {
-            let portal =
-                dir === NORTH || dir === WEST
-                    ? `${grid[r + DR[dir] + DR[dir]][c + DC[dir] + DC[dir]]}${n}`
-                    : `${n}${grid[r + DR[dir] + DR[dir]][c + DC[dir] + DC[dir]]}`;
+function createGetAdjacent(recursive) {
+    return function getAdjacent([r, c], level, grid) {
+        var adj = []; // coords of neighbours that can be reached
+        var neighbors = getNeighbours([r, c], grid);
+        neighbors.forEach((n, dir) => {
+            if (n === ".") adj.push([r + DR[dir], c + DC[dir], level]);
+            if (n && n.match(/^[A-Z]$/)) {
+                let portal =
+                    dir === NORTH || dir === WEST
+                        ? `${grid[r + DR[dir] + DR[dir]][c + DC[dir] + DC[dir]]}${n}`
+                        : `${n}${grid[r + DR[dir] + DR[dir]][c + DC[dir] + DC[dir]]}`;
 
-            let P = findPortal(portal, grid);
-            if (P.length > 1) {
-                let [[r1, c1], [r2, c2]] = P;
-                r === r1 && c === c1 ? adj.push([r2, c2]) : adj.push([r1, c1]);
+                let P = findPortal(portal, grid, recursive);
+                if (P.length > 1) {
+                    let [[r1, c1, dl1], [r2, c2, dl2]] = P;
+                    if (r === r1 && c === c1) {
+                        if (level + dl1 >= 0) adj.push([r2, c2, level + dl1]);
+                    } else {
+                        if (level + dl2 >= 0) adj.push([r1, c1, level + dl2]);
+                    }
+                }
             }
-        }
-    });
-    return adj;
+        });
+        return adj;
+    };
 }
 
 var start = findPortal("AA", GRID)[0];
 var goal = findPortal("ZZ", GRID)[0];
 
-function bfs([sr, sc], [gr, gc], grid) {
-    var Q = [];
+function bfs(start, [gr, gc], grid, recursive) {
+    var getAdjacent = createGetAdjacent(recursive);
+    var Q = [{ coord: start, level: 0 }];
     var visited = new Set();
-    Q.push({ coord: [sr, sc] });
     while (Q.length > 0) {
         let v = Q.shift();
         let [vr, vc] = v.coord;
-        if (vr === gr && vc === gc) return v;
-        let adj = getAdjacent(v.coord, grid);
-        adj.forEach(([wr, wc]) => {
-            if (!visited.has(`${wr},${wc}`)) {
-                visited.add(`${wr},${wc}`);
-                Q.push({ coord: [wr, wc], previous: v });
+        if (v.level === 0 && vr === gr && vc === gc) return v;
+        let adj = getAdjacent(v.coord, v.level, grid);
+        adj.forEach(([wr, wc, level]) => {
+            if (!visited.has(`${wr},${wc},${level}`)) {
+                visited.add(`${wr},${wc},${level}`);
+                Q.push({ coord: [wr, wc], level: level, previous: v });
             }
         });
     }
 }
 
-function reconstruct_path(_current) {
+function reconstructPath(_current) {
     var current = _current;
     var total_path = [current];
     while (current.previous) {
@@ -105,6 +118,8 @@ function reconstruct_path(_current) {
     return total_path;
 }
 
-var path = reconstruct_path(bfs(start, goal, GRID));
+var path = reconstructPath(bfs(start, goal, GRID, false));
 console.log("Part 1:", path.length - 1);
-// console.log("Part 2:", findPortal("NS", GRID));
+
+var recursivePath = reconstructPath(bfs(start, goal, GRID, true));
+console.log("Part 2:", recursivePath.length - 1);

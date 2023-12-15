81e1b8dea70ca9b88ab267f88f3bd1fa816cb7c5
Uddström, Jesper (Consultant) <Jesper.Uddstrom@skandia.se>
2023-12-15 17:36:15 +0100

    day 12

--- 2023/Day12/main.js
+++ 2023/Day12/main.js
@@ -1,4 +1,4 @@
-import { getData, getPath, count, rng, sum } from '../lib/utils.js';
+import { getData, getPath, sum } from '../lib/utils.js';
 
 var PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/puzzle_input`;
 
@@ -13,55 +13,41 @@ function unfold([pattern, grps]) {
     ];
 }
 
-var ans = 0;
-var DP = new Map();
 // pi = pattern index
 // gi = group index
-// dashes = number of consecutive dashes
-function solve(pattern, grps, pi = 0, gi = 0, dashes = 0) {
+// dashes = number of consecutive '#'
+function solve(pattern, grps, pi = 0, gi = 0, dashes = 0, DP = new Map()) {
+    var key = `${pi},${gi},${dashes}`;
+    if (DP.has(key)) return DP.get(key);
+
+    var cnt = 0;
     if (pi === pattern.length) {
-        if (gi === grps.length || (gi === grps.length - 1 && grps[gi] === dashes)) ans++;
-        return;
+        if (gi === grps.length || (gi === grps.length - 1 && grps[gi] === dashes)) return 1;
+        return 0;
     }
 
-    var key = `${pi},${gi},${dashes}`; // will use this for DP later.
-
-    if (DP.has(key)) return DP.get(key);
     var p = pattern[pi];
     var g = grps[gi];
 
-    if (p === '.') {
-        if (dashes === 0) solve(pattern, grps, pi + 1, gi, 0);
-        if (dashes > 0 && dashes === g) solve(pattern, grps, pi + 1, gi + 1, 0);
-    }
-    if (p === '#') {
-        if (dashes + 1 <= g) solve(pattern, grps, pi + 1, gi, dashes + 1);
-    }
+    if (p === '.' && dashes === 0) cnt += solve(pattern, grps, pi + 1, gi, 0, DP);
+    if (p === '.' && dashes > 0 && dashes === g) cnt += solve(pattern, grps, pi + 1, gi + 1, 0, DP);
+    if (p === '#' && dashes + 1 <= g) cnt += solve(pattern, grps, pi + 1, gi, dashes + 1, DP);
     if (p === '?') {
         // Test with ".".
-        if (dashes === 0) solve(pattern, grps, pi + 1, gi, 0);
-        if (dashes > 0 && dashes === g) solve(pattern, grps, pi + 1, gi + 1, 0);
-
+        if (dashes === 0) cnt += solve(pattern, grps, pi + 1, gi, 0, DP);
+        if (dashes > 0 && dashes === g) cnt += solve(pattern, grps, pi + 1, gi + 1, 0, DP);
         // Test with "#".
-        if (dashes + 1 <= g) solve(pattern, grps, pi + 1, gi, dashes + 1);
-
-        return;
+        if (dashes + 1 <= g) cnt += solve(pattern, grps, pi + 1, gi, dashes + 1, DP);
     }
+
+    DP.set(key, cnt);
+    return cnt;
 }
 
 var data = getData(PUZZLE_INPUT_PATH)(parser);
 
-var start = Date.now();
+var p1 = sum(data.map(([pattern, grps]) => solve(pattern.split(''), grps.split(',').map(Number))));
+var p2 = sum(data.map(unfold).map(([pattern, grps]) => solve(pattern.split(''), grps.split(',').map(Number))));
 
-//solve('.??..??...?##.'.split(''), '1,1,3'.split(',').map(Number));
-//solve('???.###'.split(''), '1,1,3'.split(',').map(Number));
-//solve('?###????????'.split(''), '3,2,1'.split(',').map(Number));
-
-data.forEach(([pattern, grps]) => solve(pattern.split(''), grps.split(',').map(Number)));
-
-//data.map(unfold).forEach(([pattern, grps]) => solve(pattern.split(''), grps.split(',').map(Number)));
-
-var t = Date.now() - start;
-
-console.log('Part 1:', ans, t);
-console.log('Part 2:');
+console.log('Part 1:', p1);
+console.log('Part 2:', p2);

b200674c84130441d50b646f73bab61ff17591c0
Uddström, Jesper (Consultant) <Jesper.Uddstrom@skandia.se>
2023-12-15 17:08:45 +0100

    day 12 part 1

--- 2023/Day12/main.js
+++ 2023/Day12/main.js
@@ -6,35 +6,62 @@ function parser(input) {
     return input.split('\n').map((line) => line.split(' '));
 }
 
+function unfold([pattern, grps]) {
+    return [
+        pattern + '?' + pattern + '?' + pattern + '?' + pattern + '?' + pattern,
+        grps + ',' + grps + ',' + grps + ',' + grps + ',' + grps,
+    ];
+}
+
+var ans = 0;
+var DP = new Map();
+// pi = pattern index
+// gi = group index
+// dashes = number of consecutive dashes
+function solve(pattern, grps, pi = 0, gi = 0, dashes = 0) {
+    if (pi === pattern.length) {
+        if (gi === grps.length || (gi === grps.length - 1 && grps[gi] === dashes)) ans++;
+        return;
+    }
+
+    var key = `${pi},${gi},${dashes}`; // will use this for DP later.
+
+    if (DP.has(key)) return DP.get(key);
+    var p = pattern[pi];
+    var g = grps[gi];
+
+    if (p === '.') {
+        if (dashes === 0) solve(pattern, grps, pi + 1, gi, 0);
+        if (dashes > 0 && dashes === g) solve(pattern, grps, pi + 1, gi + 1, 0);
+    }
+    if (p === '#') {
+        if (dashes + 1 <= g) solve(pattern, grps, pi + 1, gi, dashes + 1);
+    }
+    if (p === '?') {
+        // Test with ".".
+        if (dashes === 0) solve(pattern, grps, pi + 1, gi, 0);
+        if (dashes > 0 && dashes === g) solve(pattern, grps, pi + 1, gi + 1, 0);
+
+        // Test with "#".
+        if (dashes + 1 <= g) solve(pattern, grps, pi + 1, gi, dashes + 1);
+
+        return;
+    }
+}
+
 var data = getData(PUZZLE_INPUT_PATH)(parser);
-
-function isValidateArrangement(arr, conf) {
-    return (
-        Array.from(arr.matchAll(/\#+/g), (m) => m[0])
-            .map((grp) => grp.length)
-            .join(',') === conf
-    );
-}
-
-function generateArrangements(pattern, conf) {
-    var damagedSprings = sum(conf.split(',').map(Number)) - count('#', pattern.split(''));
-    var length = pattern.match(/\?/g).length;
-    var arrs = rng(2 ** length)
-        .map((n) => n.toString(2).padStart(length, '0').split(''))
-        .filter((n) => count('1', n) === damagedSprings)
-        .map((setup) => setup.reduce((acc, curr) => acc.replace('?', curr === '1' ? '#' : '.'), pattern));
-    return arrs;
-}
 
 var start = Date.now();
 
-var setOfArrangements = data.map(
-    ([pattern, conf]) => generateArrangements(pattern, conf).filter((arr) => isValidateArrangement(arr, conf)).length
-);
+//solve('.??..??...?##.'.split(''), '1,1,3'.split(',').map(Number));
+//solve('???.###'.split(''), '1,1,3'.split(',').map(Number));
+//solve('?###????????'.split(''), '3,2,1'.split(',').map(Number));
 
-var p1 = sum(setOfArrangements);
+data.forEach(([pattern, grps]) => solve(pattern.split(''), grps.split(',').map(Number)));
+
+//data.map(unfold).forEach(([pattern, grps]) => solve(pattern.split(''), grps.split(',').map(Number)));
 
 var t = Date.now() - start;
 
-console.log('Part 1:', p1, t);
+console.log('Part 1:', ans, t);
 console.log('Part 2:');

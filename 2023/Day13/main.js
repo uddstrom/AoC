b783518ae1d8028257e7d5734127e4266191bd3b
Jesper Uddström <jesper@uddstrom.se>
2023-12-16 21:29:41 +0100

    Day 13

--- 2023/Day13/main.js
+++ 2023/Day13/main.js
@@ -5,44 +5,41 @@ var PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/puzzle_input`;
 function parser(input) {
     return input.split("\n\n").map((grid) => {
         var rows = grid.split("\n");
-        var R = rows.map((row) => parseInt(row.replaceAll("#", 1).replaceAll(".", 0), 2));
+        var R = rows.map((row) => row.replaceAll("#", 1).replaceAll(".", 0));
         var C = rng(rows[0].length)
             .map((i) => rows.map((row) => row[i]))
-            .map((cols) => parseInt(cols.join("").replaceAll("#", 1).replaceAll(".", 0), 2));
+            .map((cols) => cols.join("").replaceAll("#", 1).replaceAll(".", 0));
         return [R, C];
     });
 }
 
-function findReflection(arr) {
+function getDiff(n1, n2) {
+    var xor = (a, b) => a ^ b;
+    return n1
+        .split("")
+        .map((n, i) => xor(n, n2[i]))
+        .filter(Boolean).length;
+}
+
+function findReflection(arr, diffTolerance = 0) {
     for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] === arr[i + 1]) {
-            let k = i - 1;
-            let l = i + 2;
-            var reflection = true;
-            while (k >= 0 && l < arr.length) {
-                // console.log("Comparing", arr[k], arr[l]);
-                if (arr[k] !== arr[l]) {
-                    reflection = false;
-                    break;
-                }
-                k--;
-                l++;
-            }
-            if (reflection) return i + 1;
+        let left = i;
+        let right = i + 1;
+        var diff = 0;
+        while (left >= 0 && right < arr.length) {
+            diff += getDiff(arr[left], arr[right]);
+            left--;
+            right++;
         }
+        if (diff === diffTolerance) return i + 1;
     }
     return 0;
 }
 
 var data = getData(PUZZLE_INPUT_PATH)(parser);
 
-var p1 = sum(
-    data.map(([R, C]) => {
-        var rows = findReflection(R);
-        var cols = findReflection(C);
-        return cols + 100 * rows;
-    })
-);
+var p1 = sum(data.map(([R, C]) => findReflection(C) + 100 * findReflection(R)));
+var p2 = sum(data.map(([R, C]) => findReflection(C, 1) + 100 * findReflection(R, 1)));
 
 console.log("Part 1:", p1);
-console.log("Part 2:");
+console.log("Part 2:", p2);

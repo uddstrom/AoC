abbbbef6b47d08a54fa01c0bf10acf6ebce5da32
Jesper Uddström <jesper@uddstrom.se>
2023-12-21 19:22:19 +0100

    refactor day 17

--- 2023/Day17/main.js
+++ 2023/Day17/main.js
@@ -1,5 +1,5 @@
 import { PrioQ } from "../lib/PrioQ.js";
-import { getData, getPath, rng } from "../lib/utils.js";
+import { getData, getPath, sum } from "../lib/utils.js";
 
 var PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/puzzle_input`;
 
@@ -7,28 +7,23 @@ function parser(input) {
     return input.split("\n").map((line) => line.split("").map(Number));
 }
 
-function solve(grid, start, goal, h, p2) {
-    var compareFn = (n1, n2) => n1.f - n2.f;
+function solve(grid, start, goal, p2) {
+    var compareFn = (n1, n2) => n1.g - n2.g;
     var id = ({ r, c, dir, steps }) => `${r},${c},${dir},${steps}`;
 
     var openSet = new PrioQ(compareFn, id);
     var visited = new Map();
 
     start.g = 0;
-    start.f = h(start, goal);
-
     openSet.push(start);
 
     while (!openSet.empty()) {
         var current = openSet.pop();
-
         if (current.r === goal.r && current.c === goal.c) return current.g;
-
         visited.set(id(current), current.g);
 
         getNeighbors(grid, current, p2).forEach((neighbor) => {
             if (!visited.has(id(neighbor)) || neighbor.g < visited.get(id(neighbor))) {
-                neighbor.f = neighbor.g + h(neighbor, goal);
                 if (!openSet.includes(neighbor)) {
                     openSet.push(neighbor);
                 }
@@ -39,73 +34,44 @@ function solve(grid, start, goal, h, p2) {
 
 function getNeighbors(grid, node, p2) {
     var inGrid = (r, c) => r >= 0 && r < ROWS && c >= 0 && c < COLS;
-    var DR = [-1, 0, 1, 0];
-    var DC = [0, 1, 0, -1];
-    var N = rng(4).map((dir) => {
-        if (p2) {
-            // Part 2
-            let r = node.r,
-                c = node.c,
-                g = node.g;
+    return [
+        [-1, 0], // up
+        [0, 1], // right
+        [1, 0], // down
+        [0, -1], // left
+    ]
+        .map(([dr, dc], dir) => {
+            var r = node.r + dr;
+            var c = node.c + dc;
+            var g = node.g + (grid[r] && grid[r][c]);
+            var steps = node.dir === dir ? node.steps + 1 : 1;
 
-            if (node.dir === dir && node.steps < 10) {
-                r += DR[dir];
-                c += DC[dir];
-                if (inGrid(r, c)) {
-                    g += grid[r][c];
-                    return { r, c, dir, steps: node.steps + 1, g };
-                }
-            }
-            if (dir !== node.dir && dir !== (node.dir + 2) % 4) {
-                rng(4).forEach((_) => {
-                    r += DR[dir];
-                    c += DC[dir];
-                    g += inGrid(r, c) ? grid[r][c] : 0;
-                });
-                if (inGrid(r, c)) {
-                    return { r, c, dir, steps: 4, g };
-                }
-            }
-        } else {
-            // Part 1
-            if ((dir !== node.dir && dir !== (node.dir + 2) % 4) || (node.dir === dir && node.steps < 3)) {
-                let r = node.r + DR[dir];
-                let c = node.c + DC[dir];
-                if (inGrid(r, c)) {
-                    return {
-                        r,
-                        c,
-                        dir,
-                        steps: node.dir === dir ? node.steps + 1 : 1,
-                        g: node.g + grid[r][c],
-                    };
-                }
-            }
-        }
-    });
-    return N.filter(Boolean);
+            // part 2, changeing direction -> take 4 steps at once.
+            var rr = node.r + dr * 4;
+            var cc = node.c + dc * 4;
+            var gg = g + sum([1, 2, 3].map((i) => grid[r + i * dr] && grid[r + i * dr][c + i * dc]));
+
+            var isntReverse = dir !== (node.dir + 2) % 4;
+            var isValidStepP1 = inGrid(r, c) && (node.steps < 3 || dir !== node.dir);
+            var isValidStepP2a = inGrid(r, c) && dir === node.dir && steps <= 10;
+            var isValidStepP2b = inGrid(rr, cc) && dir !== node.dir && node.steps >= 4;
+
+            if (!p2 && isntReverse && isValidStepP1) return { r, c, dir, steps, g };
+            if (p2 && isntReverse && isValidStepP2a) return { r, c, dir, steps, g };
+            if (p2 && isntReverse && isValidStepP2b) return { r: rr, c: cc, dir, steps: 4, g: gg };
+        })
+        .filter(Boolean);
 }
 
 var grid = getData(PUZZLE_INPUT_PATH)(parser);
 var ROWS = grid.length;
 var COLS = grid[0].length;
 
-var start = {
-    r: 0,
-    c: 0,
-    dir: 0,
-    steps: 0,
-    g: 0,
-};
-
-var goal = {
-    r: ROWS - 1,
-    c: COLS - 1,
-};
+var start = { r: 0, c: 0, dir: 1, steps: 0, g: 0 };
+var goal = { r: ROWS - 1, c: COLS - 1 };
 
 var startTime = Date.now();
-var h = (node, goal) => Math.abs(node.r - goal.r) + Math.abs(node.c - goal.c);
-var [p1, p2] = [false, true].map((p2) => solve(grid, start, goal, h, p2));
+var [p1, p2] = [false, true].map((p2) => solve(grid, start, goal, p2));
 
 console.log("Part 1:", p1);
 console.log("Part 2:", p2);

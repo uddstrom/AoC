2ccfa088f4ce1033982a6fc4216d8de64c026298
Jesper Uddström <jesper@uddstrom.se>
2025-01-04 19:23:02 +0100

    day 20 updates

--- 2024/Day18/main.js
+++ 2024/Day18/main.js
@@ -1,6 +1,6 @@
 import { makeGrid } from '../lib/grid.js';
 import { getData, getPath, range, rng } from '../lib/utils.js';
-import { PrioQ } from '../lib/PrioQ.js';
+import { HeapQ } from '../lib/HeapQ.js';
 
 var PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/${process.argv[2] ?? 'puzzle_input'}`;
 
@@ -15,8 +15,7 @@ function path(v, p = []) {
     return path(v.parent, [v, ...p]);
 }
 
-function dijk(bytesToSimulate) {
-    var prioFn = (v, w) => v.dist - w.dist;
+function bfs(bytesToSimulate) {
     var id = ({ x, y }) => `${x},${y}`;
     var neighbors = ({ x, y, dist }, G) => {
         return [[-1, 0], [0, 1], [1, 0], [0, -1]]
@@ -35,13 +34,13 @@ function dijk(bytesToSimulate) {
     });
 
     var start = { x: 0, y: 0, dist: 0 };
-    var Q = new PrioQ(prioFn, id);
+    var Q = [];
     var V = new Map();
     Q.push(start);
     V.set(id(start), start);
 
-    while (!Q.empty()) {
-        let v = Q.pop();
+    while (Q.length > 0) {
+        let v = Q.shift();
         if (v.x === 70 && v.y === 70) return path(v);
         neighbors(v, G).forEach((w) => {
             if (!V.has(id(w)) || w.dist < V.get(id(w)).dist) {
@@ -55,12 +54,12 @@ function dijk(bytesToSimulate) {
 
 function findFirstBlocker() {
     for (let i of range(1025, data.length)) {
-        if (dijk(i) === undefined) {
+        if (bfs(i) === undefined) {
             let [x, y] = data[i - 1];
             return `${x},${y}`;
         }
     }
 }
 
-console.log('Part 1:', dijk(1024).length);
+console.log('Part 1:', bfs(1024).length);
 console.log('Part 2:', findFirstBlocker());

f46fef8c8e0c1b9e14b3262f17a24bdcdddc3c61
Jesper Uddström <jesper@uddstrom.se>
2025-01-11 17:34:18 +0100

    day 23

--- 2024/Day23/main.js
+++ 2024/Day23/main.js
@@ -4,86 +4,65 @@ import { trampoline } from '../lib/fn.js';
 var PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/${process.argv[2] ?? 'puzzle_input'}`;
 
 function parser(input) {
-    return input.split('\n').map(line => line.split("-"));
+    var G = new Map();
+    input.split('\n').map(line => line.split("-")).forEach(([n, m]) => {
+        if (!G.has(n)) G.set(n, new Set());
+        if (!G.has(m)) G.set(m, new Set());
+        G.get(n).add(m);
+        G.get(m).add(n);
+    });
+    return G;
 }
 
-Array.prototype.has = function(el) {
-    return this.some(e => e === el);
-}
+var G = getData(PUZZLE_INPUT_PATH)(parser);
 
-var data = getData(PUZZLE_INPUT_PATH)(parser);
-
-var getLongest = (list) => list.sort((arr1, arr2) => arr1.length - arr2.length).pop();
-var allConnections = (a) => data.filter(pair => pair.has(a)).map(([x, y]) => x === a ? y : x);
-var areConnected = (a, b) => data.some(pair => pair.has(a) && pair.has(b));
+var largest = (list) => list.sort((arr1, arr2) => arr1.length - arr2.length).pop();
+var areConnected = (a, b) => G.get(a).has(b);
 
 var DP = new Map();
 function largestSet(a, connectionsOfA) {
-    if (DP.has(`${a},${connectionsOfA.sort().join()}`)) {
-        return DP.get(`${a},${connectionsOfA.sort().join()}`);
-    }
-    if (connectionsOfA.length === 0) return [a];
-    if (areAllConnected(connectionsOfA)) {
-        return [a, ...connectionsOfA];
-    }
-    var list = connectionsOfA.map(b => {
-        var x = computerMap.get(b).filter(c => connectionsOfA.has(c)); // connections of head that is also in tail
+    var id = `${a},${[...connectionsOfA].sort().join()}`;
+    if (DP.has(id)) return DP.get(id);
+    if (connectionsOfA.size === 0) return [a];
+    if (areAllConnected(connectionsOfA)) return [a, ...connectionsOfA];
+
+    var list = [...connectionsOfA].map(b => {
+        var x = new Set([...G.get(b)].filter(c => connectionsOfA.has(c))); // connections of head that is also in tail
         return largestSet(b, x);
     });
-    var res = [a, ...getLongest(list)];
-    DP.set(`${a},${connectionsOfA.sort().join()}`, res);
+    var res = [a, ...largest(list)];
+    DP.set(id, res);
+
     return res;
 }
 
-
-var DP1 = new Map();
 var areAllConnected = trampoline(function areAllConnected(computers) {
-    if (DP1.has(computers.sort().join())) {
-        return DP1.get(computers.sort().join());
+    if (computers.size === 1) return true;
+
+    var C = [...computers];
+    if (computers.size === 2) {
+        return areConnected(C[0], C[1]);
     }
-    var [head, ...tail] = computers;
-    if (tail.length === 0) {
-        return true;
-    }
-    if (tail.length === 1) {
-        return areConnected(head, tail[0]);
-    }
+
+    var [head, ...tail] = C;
     for (let c of tail) {
-        if (!areConnected(head, c)) {
-            DP1.set(computers.sort().join(), false);
-            return false;
-        }
+        if (!areConnected(head, c)) return false;
     }
-    return () => areAllConnected(tail);
+
+    return () => areAllConnected(new Set(tail));
 });
 
-var interconnected = [];
-data.forEach(([a, b]) => {
-    var A = data.filter((pair) => pair.has(a));
-    A.forEach(pair => {
-        var c = pair.find(comp => comp !== a && comp !== b);
-        if (areConnected(b, c)) {
-            interconnected.push([a, b, c]);
-        }
+var interconnected = new Set();
+G.keys().forEach((a) => {
+    let EA = G.get(a);
+    EA.forEach((b) => {
+        G.get(b).forEach((c) => {
+            if (EA.has(c) && c !== a) {
+                interconnected.add([a, b, c].sort().join());
+            }
+        });
     });
 });
 
-interconnected = interconnected.map(computers => computers.sort().join());
-var distinct = [...new Set(interconnected)];
-var T = distinct.filter(str => str.match(/^t|,t/));
-
-
-var C = [...new Set(data.flatMap(pair => pair))]; // the set of distinct computers
-var computerMap = new Map(C.map(c => [c, allConnections(c)]));
-
-var x = C.map((a, i) => {
-    console.log(i);
-    return largestSet(a, computerMap.get(a));
-});
-
-console.log('Part 1:', T.length);
-console.log('Part 2:', getLongest(x).sort().join());
-
-
-
-
+console.log('Part 1:', [...interconnected].filter(str => str.match(/^t|,t/)).length);
+console.log('Part 2:', largest([...G.keys()].map((n) => largestSet(n, G.get(n)))).sort().join());
